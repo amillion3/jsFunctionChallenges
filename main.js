23ec914a238e00c0c6f74ff481ac3f58e900ac4f
Andy <amillion3@gmail.com>
2018-03-04 21:50:54 -0600

    challenge 1 complete

--- main.js
+++ main.js
@@ -9,7 +9,30 @@ function writeToDom(domString, domId) {
 // ----------------------------------------------------------
 // --- Challenge 1 ------------------------------------------
 // ----------------------------------------------------------
+function challenge1 (yearBirth, yearCount) {
+  if (yearCount < yearBirth) {
+    var bornInYears = notBornYet(yearBirth, yearCount);
+  }
+  else if (yearCount === yearBirth) {
+    writeToDom("<h3>Challenge 1</h3>" + "<p>You were born this very year!</p>", "challenge-1");
+  }
+  else {
+    var ageInYears = alreadyBorn(yearBirth, yearCount);
+  }
+}
 
+function notBornYet (a, b) {  //Person not born yet
+  writeToDom("<h3>Challenge 1</h3>" + "<p>You will be born in " + (a - b) + " year(s).</p>",
+            "challenge-1");
+}
+function alreadyBorn (c, d) {  //Person already born
+  writeToDom("<h3>Challenge 1</h3>" + "<p>You are " + (d - c) + " year(s) old.</p>",
+            "challenge-1");
+}
+
+challenge1(2000, 2010); // You are 10 years old.
+// challenge1(2000, 1995); // You will be born in 5 years.
+// challenge1(2000, 2000); // You were born this very year!
 
 // ----------------------------------------------------------
 // --- Challenge 2 ------------------------------------------
